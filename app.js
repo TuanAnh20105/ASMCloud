079b9d213b64dcb43ee75fc2a107169f83b3c46b
TuanAnh055201 <anhntgch190543@fpt.edu.vn>
2021-08-17 11:36:44 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,93 @@
-const EXPRESS= require('express')
-const app = EXPRESS();
-app.get('/' , (req , res)=>{
+const EXPRESS = require('express')
+const { Int32} = require('mongodb');
+//https://downloads.mongodb.com/compass/mongodb-compass-1.28.1-win32-x64.exe
+const session = require('express-session')
 
-   res.end('hello from simple server :)')
+const {checkUserRole, insertUser,insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentById,updateStudent} = require('./databaseHandler');
 
+const APP = EXPRESS()
+
+// Use the session middleware
+APP.use(session({ secret: '124447yd@@$%%#', cookie: { maxAge: 60000 },saveUninitialized:false,resave:false}))
+
+APP.use(EXPRESS.urlencoded({extended:true}))
+APP.set('view engine','hbs')
+
+APP.post('/update',async (req,res)=>{
+    const id = req.body.id;
+    const nameInput = req.body.txtName;
+    const tuoiInput = req.body.txtTuoi;
+    await updateStudent(id,nameInput,tuoiInput);
+    res.redirect('/');
 })
-const PORT = process.env.PORT ||5000;
-app.listen(PORT);
-console.log("running");
+
+APP.get('/edit',async (req,res)=>{
+    const idInput = req.query.id;
+    const search_Student = await getStudentById(idInput);
+    res.render('edit',{student:search_Student})
+})
+APP.get('/login',(req,res)=>{
+    res.render('login')
+})
+APP.post('/doLogin',async (req,res)=>{
+    const nameInput = req.body.txtName;
+    const passInput = req.body.txtPassword;
+    const userRole = await checkUserRole(nameInput,passInput);
+    if(userRole!="-1"){
+        req.session["User"]={
+            name : nameInput,
+            role : userRole
+        }
+    }
+    res.redirect('/');
+})
+
+APP.post('/insert',async (req,res)=>{
+    const nameInput = req.body.txtName;
+    const tuoiInput = req.body.txtTuoi;
+    const pictureInput = req.body.picture;
+    const newStudent = {name:nameInput,tuoi: Int32(tuoiInput),picture:pictureInput};
+    await insertStudent(newStudent);
+    //chuyen huong den file Index
+    res.redirect('/');
+})
+APP.get('/delete',async (req,res)=>{
+    const idInput = req.query.id;
+    await deleteStudent(idInput);
+    res.redirect('/');
+})
+APP.post('/search',async (req,res)=>{
+    const searchInput = req.body.txtSearch;
+    const allStudents = await searchStudent(searchInput);
+    res.render('index',{data:allStudents})
+})
+
+APP.get('/' ,requiresLogin, async (req,res)=>{
+    // if(!req.session["User"])
+    //     res.redirect('/login')
+    const allStudents = await getAllStudent();
+    res.render('index',{data:allStudents,user:req.session["User"]})
+})
+APP.post('/register',async (req,res)=>{
+    const nameInput = req.body.txtName;
+    const passInput = req.body.txtPassword;
+    const roleInput = req.body.role;
+    await insertUser({name:nameInput,pass:passInput,role:roleInput})
+    res.redirect('/login')
+})
+
+APP.get('/nologin',requiresLogin, (req,res)=>{
+    res.render('noLogin');
+})
+
+//custom middleware
+function requiresLogin(req,res,next){
+    if(req.session["User"]){
+        return next()
+    }else{
+        res.redirect('/login')
+    }
+}
+
+const PORT = process.env.PORT || 5000;
+APP.listen(PORT);
---
+++ databaseHandler.js
@@ -0,0 +1,61 @@
+const {MongoClient,ObjectId} = require('mongodb');
+
+const URL = 'mongodb://localhost:27017';
+
+const DATABASE_NAME = "GCH0803DB"
+
+async function getDB() {
+    const client = await MongoClient.connect(URL);
+    const dbo = client.db(DATABASE_NAME);
+    return dbo;
+}
+
+async function insertUser(user) {
+    const dbo = await getDB();
+    const newS = await dbo.collection("users").insertOne(user);
+    console.log("Gia tri id moi duoc insert la: ", newS.insertedId.toHexString());
+}
+
+async function insertStudent(newStudent) {
+    const dbo = await getDB();
+    const newS = await dbo.collection("students").insertOne(newStudent);
+    console.log("Gia tri id moi duoc insert la: ", newS.insertedId.toHexString());
+}
+
+async function deleteStudent(idInput) {
+    const dbo = await getDB();
+    await dbo.collection("students").deleteOne({ _id: ObjectId(idInput) });
+}
+async function searchStudent(searchInput) {
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({ name: searchInput }).toArray();
+    return allStudents;
+}
+async function getAllStudent() {
+    const dbo = await getDB();
+    const allStudents = await dbo.collection("students").find({}).toArray();
+    return allStudents;
+}
+
+async function getStudentById(idInput){
+    const dbo = await getDB();
+    return dbo.collection("students").findOne({_id:ObjectId(idInput)});
+}
+
+async function updateStudent(id,nameInput,tuoiInput){
+    const dbo = await getDB();
+    dbo.collection("students").updateOne({_id:ObjectId(id)},{$set:{name:nameInput,tuoi:tuoiInput}})
+
+}
+async function checkUserRole(nameI,passI){
+    const dbo = await getDB();
+    const user= await dbo.collection("users").findOne({name:nameI,pass:passI});
+    if (user==null) {
+        return "-1"
+    }else{
+        console.log(user)
+        return user.role;
+    }
+}
+
+module.exports = {getDB,insertStudent,deleteStudent,searchStudent,getAllStudent,getStudentById,updateStudent,insertUser,checkUserRole}
