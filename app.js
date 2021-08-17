af630fd0e875e5224385cf601a8cd1cb03598275
TuanAnh055201 <anhntgch190543@fpt.edu.vn>
2021-08-17 11:15:16 +0700

    update

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");console.log("Hello");
-console.log("Hello");
-console.log("Hello");
-console.log("Hello");
+const EXPRESS= require('express')
+const app = EXPRESS();
+app.get('/' , (req , res)=>{
+
+   res.end('hello from simple server :)')
+
+})
+const PORT = process.env.PORT ||5000;
+app.listen(PORT);
+console.log("running");
